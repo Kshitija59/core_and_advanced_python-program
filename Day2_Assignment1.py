9309db7372e0a67a69307bd32f0fd50b07dd4507
Kshitija59 <157447435+Kshitija59@users.noreply.github.com>
2025-02-11 18:00:55 +0530

    Add files via upload

---
+++ Day2_Assignment1.py
@@ -0,0 +1,20 @@
+# Define two numbers
+num1 = 5
+num2 = 3
+
+# Calculate the product of num1 and num2
+product = num1 * num2  # Multiply the numbers
+
+# Calculate the sum of num1 and num2
+sum_result = num1 + num2  # Add the numbers
+
+# Output the results
+print("The product of num1 and num2 is: product")
+print("The sum of num1 and num2 is: sum_result")
+
+
+
+Output=>
+""" The product of 5 and 3 is: 15
+The sum of 5 and 3 is: 8
+"""
---
+++ Day2_Assignment2.py
@@ -0,0 +1,14 @@
+# Declare two variables
+num1 = 8
+num2 = 12
+
+# Use ternary operator to find and print the largest number
+largest = num1 if num1 > num2 else num2  # Check which number is larger
+
+# Output the result
+print("The largest number between num1 and num2 is: largest")
+
+
+"""
+The largest number between 8 and 12 is: 12
+"""
