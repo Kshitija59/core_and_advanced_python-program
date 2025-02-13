3726fa66efa0199d2652e543b4d89da1d8062019
Kshitija59 <157447435+Kshitija59@users.noreply.github.com>
2025-02-13 12:56:30 +0530

    Add files via upload

---
+++ Day5_Assignment1.py
@@ -0,0 +1,14 @@
+# Define the div function that takes two parameters (a and b)
+def div(a, b):
+    # Perform division and return the result
+    return a / b
+
+# Call the function with two numbers and store the result
+result = div(10, 2)
+
+# Display the result
+print("The result of division is:", result)
+
+#Output
+"The result of division is: 5.0"
+
---
+++ Day5_Assignment2.py
@@ -0,0 +1,20 @@
+"""2. Declare a square() function with one parameter.
+Then call the function and pass one number and display the square of that number ."""
+
+
+# Define the square function that takes one parameter (n)
+def square(n):
+    # Calculate the square of the number and return the result
+    return n * n
+
+# Call the function with one number and store the result
+result = square(20)
+
+# Display the result
+print("The square of the number is:", result)
+
+
+
+#Output
+"The square of the number is: 400"
+
